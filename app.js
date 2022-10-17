2bd02605066feeb6185ec8413d7f4e0213de9ef0
Krishna <krish.biruduraju@gmail.com>
2022-10-17 13:56:25 +0000

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
+
+app.use(express.json());
 
 const dbPath = path.join(__dirname, "twitterClone.db");
 
@@ -32,7 +36,7 @@ const isShortPassword = (password) => {
 
 // API 1 //
 
-app.post("/users/", async (request, response) => {
+app.post("/register/", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
   const hashedPassword = await bcrypt.hash(request.body.password, 10);
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
@@ -60,5 +64,50 @@ app.post("/users/", async (request, response) => {
   } else {
     response.status = 400;
     response.send("User already exists");
+  }
+});
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        next();
+      }
+    });
+  }
+};
+
+// API 2 //
+
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
   }
 });
